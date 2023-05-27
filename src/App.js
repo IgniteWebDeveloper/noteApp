b784b63ae818452210f5b3c900183919ee1f5ca6
Syed Adnan Ali <61422258+IgniteWebDeveloper@users.noreply.github.com>
2023-05-27 14:30:57 +0530

    fkndkj

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import React, { useState } from 'react';
-import { Route, Routes, useNavigate } from 'react-router-dom';
-import Home from './Components/Home.js';
-import Create from './Components/Create.js';
-import Update from './Components/Update.js';
-import Trash from './Components/Trash.js';
-import Nav from './Components/Nav';
-import uuid from 'uuid-v4';
-import { FourGMobiledata } from '@mui/icons-material';
+// import React, { useState } from 'react';
+// import { Route, Routes, useNavigate } from 'react-router-dom';
+// import Home from './Components/Home.js';
+// import Create from './Components/Create.js';
+// import Update from './Components/Update.js';
+// import Trash from './Components/Trash.js';
+// import Nav from './Components/Nav';
+// import uuid from 'uuid-v4';
+// import { FourGMobiledata } from '@mui/icons-material';
 
 
-const App = () => {
+// const App = () => {
   const [note, noteList] = useState([
     {
       id: uuid(),
@@ -18,10 +18,10 @@ const App = () => {
     }
   ])
 
-  const [updateNote, updateList] = useState(null);
+//   const [updateNote, updateList] = useState(null);
 
-  const [trashNote, trashList] = useState([]);
-  const navigate = useNavigate();
+//   const [trashNote, trashList] = useState([]);
+//   const navigate = useNavigate();
 
 
   const onNewNoteHandler = (event) => {
@@ -33,18 +33,18 @@ const App = () => {
     navigate('/')
   };
 
-  const onupdateNote = (id) => {
-    navigate('/update')
-    // const copyNote = [...updateNote]
-    // const noteIndex = note.findIndex(note => note.id === id)
-    // updateNote.splice(noteIndex, 1)
-    // updateList(updateNote)
+//   const onupdateNote = (id) => {
+//     navigate('/update')
+//     // const copyNote = [...updateNote]
+//     // const noteIndex = note.findIndex(note => note.id === id)
+//     // updateNote.splice(noteIndex, 1)
+//     // updateList(updateNote)
     
-    const foundNote = note.find(note => note.id === id)
-    updateList(foundNote)
-    // updateNote.push(foundNote)
-    // updateList(updateNote)
-  };
+//     const foundNote = note.find(note => note.id === id)
+//     updateList(foundNote)
+//     // updateNote.push(foundNote)
+//     // updateList(updateNote)
+//   };
 
   const onDeleteNote = (id) =>{
     const noteIndex = note.findIndex(note => note.id === id)
@@ -56,40 +56,69 @@ const App = () => {
     trashNote.push(trashedNote)
     console.log(trashNote);
   };
-  const onPermanentDelete = (id)=> {
-    const noteIndex = trashNote.findIndex(trashNote => trashNote.id === id)
-    const copyNote = [...trashNote]
-    copyNote.splice(noteIndex, 1)
-    trashList(copyNote)
-    console.log(copyNote);
-  };
+//   const onPermanentDelete = (id)=> {
+//     const noteIndex = trashNote.findIndex(trashNote => trashNote.id === id)
+//     const copyNote = [...trashNote]
+//     copyNote.splice(noteIndex, 1)
+//     trashList(copyNote)
+//     console.log(copyNote);
+//   };
 
-  const onRestoreNote = (id) => {
-    const foundNote = trashNote.find(trashNote => trashNote.id === id)
-    const copyNote = [...trashNote]
-    copyNote.push(foundNote)
-    noteList(copyNote)
+//   const onRestoreNote = (id) => {
+//     const foundNote = trashNote.find(trashNote => trashNote.id === id)
+//     const copyNote = [...trashNote]
+//     copyNote.push(foundNote)
+//     noteList(copyNote)
 
 
-    console.log(foundNote)
-  };
-  const onUpddateNote = () => {
-    console.log('first');
-  }
+//     console.log(foundNote)
+//   };
+//   const onUpddateNote = () => {
+//     console.log('first');
+//   }
 
-  return <div>
-    <Nav />
-    <Routes>
-      <Route
-        exact
-        path={`/`}
-        element={<Home ome notes={note} updatingNote={onupdateNote} deleteNote={onDeleteNote} />}
-      />
+//   return <div>
+//     <Nav />
+//     <Routes>
+//       <Route
+//         exact
+//         path={`/`}
+//         element={<Home ome notes={note} updatingNote={onupdateNote} deleteNote={onDeleteNote} />}
+//       />
       <Route exact path={`/create`} element={<Create newNote={onNewNoteHandler} />} />
-      <Route exact path={`/update`} element={<Update updateNote={updateNote} onupdatingNote={onUpddateNote}/>} />
-      <Route exact path={`/trash`} element={<Trash trashNote= {trashNote} permanentDelete={onPermanentDelete} restoreNote={onRestoreNote}/>} />
-    </Routes>
-  </div>
-};
+//       <Route exact path={`/update`} element={<Update updateNote={updateNote} onupdatingNote={onUpddateNote}/>} />
+//       <Route exact path={`/trash`} element={<Trash trashNote= {trashNote} permanentDelete={onPermanentDelete} restoreNote={onRestoreNote}/>} />
+//     </Routes>
+//   </div>
+// };
 
-export default App;
+// export default App;
+import React, { useState } from 'react';
+import MUIDataTable from "mui-datatables";
+
+const App = () => {
+  const columns = ["Name", "Company", "City", "State"];
+  const [responsive, setResponsive] = useState('vertical');
+
+const data = [
+ ["Joe James", "Test Corp", "Yonkers", "NY"],
+ ["John Walsh", "Test Corp", "Hartford", "CT"],
+ ["Bob Herm", "Test Corp", "Tampa", "FL"],
+ ["James Houston", "Test Corp", "Dallas", "TX"],
+];
+
+const options = {
+  filterType: 'checkbox',
+  responsive
+};
+  return (
+    <MUIDataTable
+  title={"Employee List"}
+  data={data}
+  columns={columns}
+  options={options}
+/>
+  )
+}
+
+export default App
